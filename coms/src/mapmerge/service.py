1bc76146d2232de69e5218a6fa80a11ea947ab6b
Connor Mclaughlin <cjmclaughlin@wpi.edu>
2022-02-22 14:29:05 -0500

    hough attempt

--- coms/src/mapmerge/service.py
+++ coms/src/mapmerge/service.py
@@ -1,10 +1,11 @@
+from mapmerge.hough_merge import hough_mapmerge
 from mapmerge.merge_utils import pad_maps
 import numpy as np
 from mapmerge.keypoint_merge import sift_mapmerge, orb_mapmerge
 from mapmerge.merge_utils import resize_map, combine_aligned_maps, acceptance_index
 
 
-def mapmerge_pipeline(map1, map2, method="sift", scale_fix=False):
+def mapmerge_pipeline(map1, map2, method="hough", scale_fix=False):
     """
     end-to-end map merge pipeline for testing
     
@@ -14,7 +15,13 @@ def mapmerge_pipeline(map1, map2, method="sift", scale_fix=False):
     - method: one of ["sift", "orb"]
     - scale_fix: boolean, whether or not to attempt scale-adjusted merge
     """
-    merge_fn = sift_mapmerge if method == "sift" else orb_mapmerge
+    merge_fn = None
+    if method == "sift":
+        merge_fn = sift_mapmerge 
+    elif method == "orb":
+        merge_fn = orb_mapmerge
+    else:
+        merge_fn = hough_mapmerge
     map1, map2 = pad_maps(map1, map2)
     if scale_fix:
         for scale in [0.95, 0.975, 1, 1.025, 1.05]:
